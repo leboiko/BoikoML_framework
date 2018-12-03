3972b763a7321a3d4fdae526e31102060bfb3f1c
Luis Eduardo Boiko Ferreira <luiseduardoboikoferreira@MacBook-Pro-de-Luis.local>
2018-12-03 16:06:31 -0200

    Pod ok

--- BoikoML/BoikoML.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BoikoML.swift
-//  BoikoML
-//
-//  Created by Luis Eduardo Boiko Ferreira on 24/09/2018.
-//  Copyright © 2018 Luis Eduardo Boiko Ferreira. All rights reserved.
-//
-
-import Foundation
